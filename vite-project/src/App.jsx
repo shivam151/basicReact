51fa97deb89abf51416930e66598a85c7c4fe472
shivam katiyar <shivamkati45@gmail.com>
2024-11-09 12:16:01 +0530

    chnage in the

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,28 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function add() {
+    console.log(counter, "counter");
+    setCounter(counter + 1);
+  }
+
+  function sub() {
+    console.log(counter, "counter");
+    setCounter(counter - 1);
+  }
+
   return (
     <>
-   <h1>this is the react</h1>
-   <h2></h2>
-   </>
-  )
+      <h1>Counter Value: {counter}</h1>  
+
+      <button onClick={add}>Add</button>
+      <br />
+      <button onClick={sub}>Subtract</button>
+    </>
+  );
 }
-export default App
+
+export default App;
